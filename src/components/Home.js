760f879f0269686567f0c20d62556fc154eba272
david-mcgrouther <dbmcgrouther@gmail.com>
2020-09-24 21:03:19 -0700

    made some edits to homepage. links to sign up and sign in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
     return (
-        <h1>
-            Welcome to Evently
-        </h1>
+        <div>
+            <div className="home-page__header-section">
+                <h1>
+                    Welcome to Evently!
+                </h1>
+                <p>A website to schedule your favorite virtual tabletop gaming sessions</p>
+            </div>
+            <div>
+                <p>
+                    Have an account already? Login <Link to={'/Login'}>here</Link>.
+                </p>
+                <p>
+                    If not, sign up <Link to={'/Register'}>here</Link>!
+                </p>
+            </div>
+        </div>
     );
 };
 
