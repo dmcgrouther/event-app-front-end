ee07ffc22be1ffafb23d3a33ee3cc71e688b9fac
david-mcgrouther <dbmcgrouther@gmail.com>
2020-10-06 15:47:58 -0700

    got a basic list to render on page. git commit before checking out using a an actual html table

--- src/components/EventListPage/EventListPageListOfEvents.js
+++ src/components/EventListPage/EventListPageListOfEvents.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import './EventListPageListOfEvents.css'
 
-const EventListPageListOfEvents = () => {
+let events = [
+    {
+        eventName: 'Minataur Rage!',
+        gameSystem: 'Dungeons & Dragons',
+        // gameEdition: '5th Edition',
+        meetupType: 'Online',
+        eventDate: '10-1-2020 7pm PST',
+        numberOfPlayers: '2 of 5',
+    },
+    {
+        eventName: 'Grimark Adeventures!!',
+        gameSystem: 'Dark Heresy',
+        // gameEdition: '1st Edition',
+        meetupType: 'Online',
+        eventDate: '10-1-2020 7:30pm PST',
+        numberOfPlayers: '5 of 5',
+    },
+    {
+        eventName: 'Super heroes!',
+        gameSystem: 'Fate Core',
+        // gameEdition: '1st Edition',
+        meetupType: 'Online',
+        eventDate: '10-2-2020 10:00pm PST',
+        numberOfPlayers: '3 of 5',
+    }
+];
+
+const Event = (props) => {
     return (
-        <h3>
-            stuff!
-        </h3>
+        <>
+            <div className="event">
+                <div>{props.eventName}</div>
+                <div>{props.gameSystem}</div>
+            </div>
+        </>
+    )
+}
+
+const EventListPageListOfEvents = (props) => {
+    let eventList = events;
+    let listItems = eventList.map((event) => {
+        return (
+            <Event
+                eventName={event.eventName}
+                gameSystem={event.gameSystem}
+            />
+        );
+    })
+
+    return (
+        <>
+            {listItems}
+        </>
     )
 }
 
