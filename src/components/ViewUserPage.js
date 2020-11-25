49285027269bacad94353a595d27db601ec37475
david-mcgrouther <dbmcgrouther@gmail.com>
2020-11-25 14:32:04 -0800

    worked on viewUserPage. if you are logged in annd view the link for your user page , you can edit your contact info and user name. if you view someone elses user link, it shows their info. and you cannot edit it.

--- src/components/ViewUserPage.js
+++ src/components/ViewUserPage.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class ViewUserPage extends Component {
-    state = {
-        name: '',
-        contactInfo: '',
-        profilePicture: '',
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            contactInfo: '',
+            profilePicture: '',
+        }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleContactInfoChange = this.handleContactInfoChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount () {
@@ -20,23 +26,60 @@ class ViewUserPage extends Component {
           .catch((err) => console.log(err));
     }
 
-    // handleNameChange(event){
-    //     this.setState({name: event.target.value})
-    //     console.log(this.state.name)
-    // }
+    handleNameChange(event){
+        this.setState({name: event.target.value})
+        console.log(this.state.name)
+    }
+
+    handleContactInfoChange(event){
+        this.setState({contactInfo: event.target.value})
+        console.log(this.state.contactInfo)
+    }
     
+    handleSubmit(event){
+        event.preventDefault();
+        console.log(this.state.name);
+        console.log(this.state.contactInfo);
+        axios.put(`${process.env.REACT_APP_API_URL}/users/${window.location.pathname.split('/')[2]}`, {
+            name: this.state.name,
+            contactInfo: this.state.contactInfo
+        }).then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error)
+        });
+    }
+
     render () {
         if(this.props.currentUser === window.location.pathname.split('/')[2]){
+            // return (
+            //     <>
+            //         <h1>Your username is: {this.state.name}</h1>
+            //         <h3>If you would like to edit your username, do so below:</h3>
+            //         <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+            //         <button onClick={this.handleNameClick}>Submit Changes</button>
+            //         <h1>User Contact Info: {this.state.contactInfo}</h1>
+            //         <h1>{this.state.profilePicture}</h1>
+            //     </>
+            // )
             return (
                 <>
                     <h1>Your username is: {this.state.name}</h1>
-                    {/* <textarea value={this.state.name} onChange={this.handleNameChange} /> */}
-                    {/* <input type="text" value={this.state.name} onChange={this.handleNameChange} /> */}
-                    <h1>User Contact Info: {this.state.contactInfo}</h1>
-                    <h1>{this.state.profilePicture}</h1>
-                    {/* {this.props.currentUser} */}
+                    <h1>Your contact information is: {this.state.contactInfo}</h1>
+                    <h4>If you would like to edit your username and contact information (not required), you may do so below.</h4>
+                    <form onSubmit={this.handleSubmit}>
+                    <label>
+                        User Name:
+                        <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+                    </label>
+                    <label>
+                        Contact Info:
+                        <input type="text" value={this.state.contactInfo} onChange={this.handleContactInfoChange} />
+                    </label>
+                    <input type="submit" value="Save Changes" />
+                    </form>
                 </>
-            )
+              );
         } else {
             return (
                 <>
