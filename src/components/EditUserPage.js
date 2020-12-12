4c5480cd657cd3de1cb51637f699390bb6349c8e
david-mcgrouther <dbmcgrouther@gmail.com>
2020-12-11 16:27:27 -0800

    made adjustments to edit user page

--- src/components/EditUserPage.js
+++ src/components/EditUserPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './EditUserPage.css'
 
 class EditUserPage extends Component {
     constructor(props){
@@ -51,25 +52,37 @@ class EditUserPage extends Component {
         if(this.props.currentUser === window.location.pathname.split('/')[2]){
             return(
                 <>
-                    <h1>You may edit your information below</h1>
-                    <form onSubmit={this.handleSubmit}>
-                    <label>
-                        User Name:
+                    <h1 className="edit-user-page-header">Edit your information</h1>
+                    <div className="edit-user-area">
+                        <form onSubmit={this.handleSubmit}>
+                        <div className="edit-user-field">
+                            <label>
+                                User Name:
+                                <br />
+                                <input type="text" value={this.state.name} required={true} onChange={this.handleNameChange} />
+                            </label>
+                        </div>
+                        {/* <label className="edit-user-field">
+                            User Name:
+                            <br />
+                            <input type="text" value={this.state.name} required={true} onChange={this.handleNameChange} />
+                        </label>
+                        <br /> */}
+                        <div className="edit-user-field">
+                            <label>
+                                Contact Info:
+                                <br />
+                                <input type="text" value={this.state.contactInfo} onChange={this.handleContactInfoChange} />
+                            </label>
+                        </div>
                         <br />
-                        <input type="text" value={this.state.name} required={true} onChange={this.handleNameChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Contact Info:
-                        <br />
-                        <input type="text" value={this.state.contactInfo} onChange={this.handleContactInfoChange} />
-                    </label>
-                    <br />
-                    <input type="submit" value="Save Changes" />
-                    </form>
-                    <p>Click <Link to={`/users/${window.location.pathname.split('/')[2]}`}>here</Link> to return to your account info page.</p>
+                        {/* <input type="submit" value="Save Changes" /> */}
+                        <button className="btn btn-primary float-right call-to-action-button" type="submit">Save Changes</button>
+                        </form>
+                        <p>Click <Link to={`/users/${window.location.pathname.split('/')[2]}`}>here</Link> to return to your account info page.</p>
 
-                    <p>*Contact information is not required, but suggested. This is in case an event host wants to contact you, or you are a host and the attendees want to contact you.*</p>
+                        <p>*Contact information is not required, but suggested. This is in case an event host wants to contact you, or you are a host and the attendees want to contact you.*</p>
+                    </div>
                 </>
             )
         } else {
